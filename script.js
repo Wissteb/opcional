ffb976c338ccc0fe6a2d9d924af38aa6646513ba
Alumno_6K <wissteb@gmail.com>
2019-04-30 13:50:51 +0200

    primero

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function hacer_pedido(){
+  
+  let mesa = document.getElementById("mesa").value;
+    
+
+    
+    let pedido = document.getElementById("pedido").value;
+
+
+ }
+ function selecionar()
+ {
+ /* Para obtener el valor */
+ let cod = document.getElementById("hamburguesa").value;
+ alert(cod);
+  
+ /* Para obtener el texto */
+ let combo = document.getElementById("hamburguesa");
+ let selected = combo.options[combo.selectedIndex].text;
+ alert(selected);
+ }
+
+ function selecionar2()
+ {
+ /* Para obtener el valor */
+let cod1 = document.getElementById("bebida").value;
+ alert(cod1);
+  
+ /* Para obtener el texto */
+let combo1 = document.getElementById("bebida");
+let selected1 = combo.options[combo.selectedIndex].text;
+ alert(precio);
+ }
